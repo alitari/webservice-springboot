e84960c2bfec1ec3b957a6e451dedcb90423685c
alitari <alexander.krieg@arcor.de>
2015-08-01 23:09:22 +0200

    initial

---
+++ src/main/java/de/alexkrieg/Application.java
@@ -0,0 +1,22 @@
+package de.alexkrieg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	@RequestMapping("/")
+	public String home() {
+		return "You have won "+System.getenv("PRICE")+" !";
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
